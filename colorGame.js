a69ebca49fca4913aba7de35fb1fdb99d3465b4e
golbanv <golbanvladislav@gmail.com>
2020-01-15 02:03:07 +0100

    Create basic logic for the game

---
+++ colorGame.js
@@ -0,0 +1,49 @@
+let colors = generateRandomColors(6);
+let squares = document.querySelectorAll(".square");
+let pickedColor = pickColor();
+let colorDisplay = document.querySelector("#colorDisplay");
+let message = document.querySelector("#message");
+let h1 = document.querySelector("h1");
+
+colorDisplay.textContent = pickedColor;
+
+for (let i = 0; i < squares.length; i++) {
+  squares[i].style.background = colors[i];
+  squares[i].addEventListener("click", function() {
+    let clickedColor = this.style.background;
+    if (clickedColor === pickedColor) {
+      message.textContent = "Correct!";
+      changeColors(clickedColor);
+      h1.style.background = pickedColor;
+    } else {
+      this.style.background = "#232323";
+      message.textContent = "Wrong, guess again!";
+    }
+  });
+}
+
+function changeColors(color) {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.background = color;
+  }
+}
+
+function pickColor() {
+  let random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num) {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
